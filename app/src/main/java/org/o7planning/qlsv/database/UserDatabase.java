3bb6331f6a4fc048986ca2cc26bbe8b5fc7bd44d
nguyencuong161199 <nguyencuong161199@gmail.com>
2021-06-10 11:16:20 +0700

    update lai 1 lan

--- app/src/main/java/org/o7planning/qlsv/database/UserDatabase.java
+++ app/src/main/java/org/o7planning/qlsv/database/UserDatabase.java
@@ -2,15 +2,29 @@ package org.o7planning.qlsv.database;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.room.AutoMigration;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
 import org.o7planning.qlsv.user;
 
-@Database(version = 2,entities = {user.class},
-        autoMigrations = [@AutoMigration(from = 1,to = 2)])
+@Database(entities = {user.class},version = 2)
 public abstract class UserDatabase extends RoomDatabase {
+    static Migration migration_from_1_to_2 = new Migration(1, 2) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+            database.execSQL("ALTER TABLE user ADD COLUMN lop TEXT");
+            database.execSQL("ALTER TABLE user ADD COLUMN gioitinh TEXT");
+            database.execSQL("ALTER TABLE user ADD COLUMN toan TEXT");
+            database.execSQL("ALTER TABLE user ADD COLUMN ly TEXT");
+            database.execSQL("ALTER TABLE user ADD COLUMN hoa TEXT");
+
+        }
+    };
     private static final String DATABASE_NAME = "user.db";
     private static UserDatabase instance;
   
