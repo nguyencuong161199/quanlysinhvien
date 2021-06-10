2868d1f2e0e5df2eea6574e027cf837dbced4199
nguyencuong161199 <nguyencuong161199@gmail.com>
2021-06-10 11:36:51 +0700

    update version 2

--- app/src/main/java/org/o7planning/qlsv/database/UserDatabase.java
+++ app/src/main/java/org/o7planning/qlsv/database/UserDatabase.java
@@ -2,15 +2,30 @@ package org.o7planning.qlsv.database;
 
 import android.content.Context;
 
-import androidx.room.AutoMigration;
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
 import org.o7planning.qlsv.user;
 //
-@Database(version = 2,entities = {user.class},
-        autoMigrations = [@AutoMigration(from = 1,to = 2)])
+@Database(entities = {user.class},version = 2)
+
 public abstract class UserDatabase extends RoomDatabase {
+
+    static Migration migration_from_1_to_2 = new Migration(1,2) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+            database.execSQL("ALTER TABLE user ADD COLUMN GRADE TEXT");
+            database.execSQL("ALTER TABLE user ADD COLUMN SEX TEXT");
+            database.execSQL("ALTER TABLE user ADD COLUMN MATH TEXT");
+            database.execSQL("ALTER TABLE user ADD COLUMN PHYSICAL TEXT");
+            database.execSQL("ALTER TABLE user ADD COLUMN CHEMISTRY TEXT");
+
+        }
+    };
     private static final String DATABASE_NAME = "user.db";
     private static UserDatabase instance;
 
@@ -18,6 +33,7 @@ public abstract class UserDatabase extends RoomDatabase {
         if (instance == null) {
             instance = Room.databaseBuilder(context.getApplicationContext(), UserDatabase.class, DATABASE_NAME)
                     .allowMainThreadQueries()
+                    .addMigrations(migration_from_1_to_2)
                     .build();
         }
         return instance;
